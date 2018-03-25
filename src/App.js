358db5b91cc31ea19411d04f95241fe45d96c3cb
Michael Allan <Michael Allan>
2018-03-25 15:52:14 +1000

    Implement counter in App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0
+    };
+
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  onIncrement() {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }));
+  }
+
+  onDecrement() {
+    this.setState(prevState => ({
+      counter: prevState.counter - 1
+    }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My counter</h1>
+        <p>{counter}</p>
+
+        <button type="button" onClick={this.onIncrement}>
+          Increment
+        </button>
+        <button type="button" onClick={this.onDecrement}>
+          Decrement
+        </button>
       </div>
     );
   }
