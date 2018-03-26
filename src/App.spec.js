387614e4cf72157e8b662b482a5649b02e4f9a88
Michael Allan <Michael Allan>
2018-03-26 11:24:49 +1100

    Unit tests for the App component's increment/decrement methods

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,17 @@
-describe('App local state', () => {
-  it('should increment the counter in state', () => {});
+import { doIncrement, doDecrement } from './App';
 
-  it('should decrement the counter in state', () => {});
+describe('App local state', () => {
+  it('should increment the counter in state', () => {
+    const state = { counter: 0 };
+    const newState = doIncrement(state);
+
+    expect(newState.counter).to.equal(1);
+  });
+
+  it('should decrement the counter in state', () => {
+    const state = { counter: 0 };
+    const newState = doDecrement(state);
+
+    expect(newState.counter).to.equal(-1);
+  });
 });
